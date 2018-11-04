277536263488d9568547d02bde198a8fdbf3b756
Jordanzuo <zuoxianqing@163.com>
2018-11-04 17:58:31 +0800

    Add new algorithms: find lowest common ancestor of a binary tree and a binary search tree

---
+++ lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if root == None or p == None or q == None or root == p or root == q:
+            return root
+        if p.val > q.val:
+            p, q = q, p
+        if root.val > p.val and root.val < q.val:
+            return root
+        if root.val > p.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return self.lowestCommonAncestor(root.right, p, q)
+        
