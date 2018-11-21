9e7ddc131a979ce225e26691b61e4e227d5ef733
Jordanzuo <zuoxianqing@163.com>
2018-11-21 17:05:18 +0800

    Optimize algorithm: maxSlidingWindows

--- maxSlidingWindow.py
+++ maxSlidingWindow.py
@@ -2,61 +2,41 @@
 
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
-        if len(nums) == 0:
-            return []
-        elif len(nums) < k:
-            return []
-        elif k == 1:
-            return nums
-        elif len(nums) == k:
-            maxValue = nums[0]
-            for num in nums:
-                if num > maxValue:
-                    maxValue = num
-            return [maxValue]
+        length = len(nums)
+        if length == 0: return []
+        if k == 1: return nums
+        if length < k: return []
 
+        # calc the maxNum and corresponding index for the first k nums
         retList = []
-        tmpList = []
-        maxValue = nums[0]
-        maxIndex = 0
-        for index, num in enumerate(nums[1:k]):
-            if num >= maxValue:
-                maxValue = num
-                maxIndex = index
-        tmpList.extend(nums[maxIndex:k])
-        retList.append(maxValue)
+        maxNum, maxIndex = nums[0], 0
 
-        nums = nums[k:]
-        if len(nums) == 0:
-            return retList
-        for num in nums:
-            if num > maxValue:
-                maxValue = num
-                tmpList.clear()
-                tmpList.append(num)
-                retList.append(maxValue)
+        # iterate the list, go one step each time
+        lower, upper = 0, 0
+        for index, num in enumerate(nums[0:]):
+            if index == 0: continue
+            if index >= k: lower += 1
+            upper = index
+
+            if upper - maxIndex < k:
+                if num >= maxNum:
+                    maxNum = num
+                    maxIndex = index
             else:
-                tmpList.append(num)
-                if len(tmpList) <= k:
-                    retList.append(maxValue)
-                    continue
-                first = tmpList.pop(0)
-                if first != maxValue:
-                    retList.append(maxValue)
+                if num >= maxNum:
+                    maxNum = num
+                    maxIndex = index
                 else:
-                    maxValue = tmpList[0]
-                    maxIndex = 0
-                    for index, num in enumerate(tmpList[1:]):
-                        if num >= maxValue:
-                            maxValue = num
-                            maxIndex = index
-                    tmpList = tmpList[maxIndex:]
-                    retList.append(maxValue)
+                    # recalc the maxNum
+                    maxNum = nums[lower]
+                    maxIndex = lower
+                    for _index, _num in enumerate(nums[lower + 1 : upper+1]):
+                        if _num > maxNum:
+                            maxNum = _num
+                            maxIndex = lower + 1 + _index
+
+            # add the maxNum into the retList
+            if index < k - 1: continue
+            retList.append(maxNum)
 
         return retList
-
-if __name__ == "__main__":
-    nums = [1, 3, -1, -3, 5, 3, 6, 7]
-    k = 3
-    s = Solution()
-    print(s.maxSlidingWindow(nums, k))
