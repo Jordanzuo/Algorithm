f175afef22bb9a048a1b18f480790402aea52424
Jordanzuo <zuoxianqing@163.com>
2018-11-23 15:38:37 +0800

    Optimize quick sort algorithm

--- sort_quick.py
+++ sort_quick.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python3
 import sys
+import random
 
 def partition(arr, low, high):
-    pivot = arr[high]
+    randNum = random.randint(1, 3)
+    if randNum == 1:
+        pivotIndex = high
+    elif randNum == 2:
+        pivotIndex = random.randint(low, high)
+    else:
+        if high - low > 3:
+            l, m, r = low, (low + high)//2, high
+            _min = min(arr[l], arr[m], arr[r])
+            _max = max(arr[l], arr[m], arr[r])
+            if arr[l] != _min and arr[l] != _max:
+                pivotIndex = l
+            if arr[m] != _min and arr[m] != _max:
+                pivotIndex = m
+            if arr[r] != _min and arr[r] != _max:
+                pivotIndex = r
+        else:
+            pivotIndex = (high + low)//2
+    pivot = arr[pivotIndex]
+
     # i points to the first unsorted item
     # j iterates all the unsorted item, and compare with the pivot, if it's less that pivot, move it to the first unsorted position to make it become sorted item. Then i increases to the next unsorted item.
     i = low
-    for j in range(low, high):
+    for j in range(low, high+1):
        if arr[j] < pivot:
-            if i != j: arr[i], arr[j] = arr[j], arr[i]
+            if i != j:
+                arr[i], arr[j] = arr[j], arr[i]
+                # If the pivot has been switched to another place, then adjust the pivotIndex
+                if i == pivotIndex: pivotIndex = j
             i = i + 1
 
     # if i doesn't point to pivot, then switch them to make pivot to the right position.(after the last sorted item)
-    if i != high: arr[i], arr[high] = arr[high], arr[i]
+    if i != pivotIndex: arr[i], arr[pivotIndex] = arr[pivotIndex], arr[i]
 
     return i
 
